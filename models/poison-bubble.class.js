7e5a3f3ccece62e08e5a1dd45186a22205f8bd2a
JiorgoV <g.vavitsas@gmail.com>
2026-08-20 21:01:34 +0200

    fix: poison bubble now flies in correct direction based on character orientation

--- models/poison-bubble.class.js
+++ models/poison-bubble.class.js
@@ -5,8 +5,8 @@ class PoisonBubble extends ThrowableObject {
      * @param {number} x Starting position on the x-axis.
      * @param {number} y Starting position on the y-axis.
      */
-    constructor(x, y) {
-        super(x, y);
+    constructor(x, y, otherDirection) {
+        super(x, y, otherDirection);
         this.loadImage('img/Alternative_Grafiken-Sharkie/Alternative Grafiken - Sharkie/1.Sharkie/4.Attack/Bubble trap/Poisoned Bubble (for whale).png');
     }
 }
